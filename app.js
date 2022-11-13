f4f7265ac25e8eea01deb3920b10244df0499b4a
Vidhi Makani <100896986+vidhimakani74@users.noreply.github.com>
2022-11-13 19:06:40 +0530

    get next and prev dat logic builded

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 
-function reverseString(strReceived){
+function reverseString(strReceived) {
     var reversedText = strReceived.split("").reverse().join("");
     return reversedText;
 }
 
-function isPalindrome(strReceived){
-    var reversedText=reverseString(strReceived);
-    return (strReceived===reversedText);
+function isPalindrome(strReceived) {
+    var reversedText = reverseString(strReceived);
+    return (strReceived === reversedText);
 }
 
-function convertDateToString(dateEnteredByUser){
+function convertDateToString(dateEnteredByUser) {
     //strDate object
-    var strDate={
-        day:"",
-        month:"",
-        year:""
+    var strDate = {
+        day: "",
+        month: "",
+        year: ""
     };
     //for day
-    if(dateEnteredByUser.day<10){
-        strDate.day="0"+dateEnteredByUser.day;
+    if (dateEnteredByUser.day < 10) {
+        strDate.day = "0" + dateEnteredByUser.day;
     }
-    else{
-        strDate.day=dateEnteredByUser.day.toString();
+    else {
+        strDate.day = dateEnteredByUser.day.toString();
     }
     //for month
-    if(dateEnteredByUser.month<10){
-        strDate.month="0"+dateEnteredByUser.month;
+    if (dateEnteredByUser.month < 10) {
+        strDate.month = "0" + dateEnteredByUser.month;
     }
-    else{
-        strDate.month=dateEnteredByUser.month.toString();
+    else {
+        strDate.month = dateEnteredByUser.month.toString();
     }
     //for year
-    strDate.year=dateEnteredByUser.year.toString();
+    strDate.year = dateEnteredByUser.year.toString();
 
     return strDate;
 
 }
 
-function dateFormates(dateEnteredByUser){
-    var dateReceived=convertDateToString(dateEnteredByUser);
+function dateFormates(dateEnteredByUser) {
+    var dateReceived = convertDateToString(dateEnteredByUser);
 
     // dateFormates
     const DDMMYYYY = dateReceived.day + dateReceived.month + dateReceived.year;
@@ -51,17 +51,112 @@ function dateFormates(dateEnteredByUser){
     return formatsOfDate;
 }
 
-function checkPalindromeForAllDateFormates(dateEnteredByUser){
+function checkPalindromeForAllDateFormates(dateEnteredByUser) {
     var dateFormateReceived = dateFormates(dateEnteredByUser);
 
-    let resultPalindrome=false;
-    for(let i=0;i<dateFormateReceived.length;i++){
-        if(isPalindrome(i)){
-            return resultPalindrome=true;
+    let resultPalindrome = false;
+    for (let i = 0; i < dateFormateReceived.length; i++) {
+        if (isPalindrome(i)) {
+            return resultPalindrome = true;
             break;
         }
-        else{
+        else {
             return resultPalindrome;
         }
     }
+}
+
+function isLeapYear(year) {
+    if (year % 400 === 0 || year % 4 === 0) {
+        return true;
+    }
+    if (year % 100 === 0) {
+        return false;
+    }
+    return false;
+}
+
+function getNextDate(dateEnteredByUser) {
+    var day = dateEnteredByUser.day + 1;
+    var month = dateEnteredByUser.month;
+    var year = dateEnteredByUser.year;
+    const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // for day
+    if (month === 2) {
+        if (isLeapYear(year)) {
+            if (day > 29) {
+                day = 1;
+                month++;
+            }
+        }
+        else {
+            if (day > 28) {
+                day = 1;
+                month++;
+            }
+        }
+    }
+    else {
+        if (day > daysInMonth[month - 1]) {
+            day = 1;
+            month++;
+        }
+    }
+
+    // for month
+    if(month>12){
+        day=1;
+        month=1;
+        year++;
+    }
+
+    return {
+        day:day,
+        month:month,
+        year:year
+    };
+
+}
+
+function getPreviosDate(dateEnteredByUser){
+    var day = dateEnteredByUser.day - 1;
+    var month = dateEnteredByUser.month;
+    var year = dateEnteredByUser.year;
+    const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // for day
+    if (month === 3) {
+        if (isLeapYear(year)) {
+            if (day < 1) {
+                day = daysInMonth[month-1];
+                month--;
+            }
+        }
+        else {
+            if (day < 1) {
+                day = daysInMonth[month-1];
+                month--;
+            }
+        }
+    }
+    else {
+        if (day < 1) {
+            day = daysInMonth[month-1];
+            month--;
+        }
+    }
+
+    // for month
+    if(month<1){
+        day=31;
+        month=12;
+        year--;
+    }
+
+    return {
+        day:day,
+        month:month,
+        year:year
+    };
 }
